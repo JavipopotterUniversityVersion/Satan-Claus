981c53c3a9a76ad9530a590e366a449732cbbee8
Lukatis16 <lucasr06@ucm.es>
2023-12-16 16:15:22 +0100

    ALGO

--- Satan Claus/Assets/Scripts/InputManager.cs
+++ Satan Claus/Assets/Scripts/InputManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting.InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private HandMovement handMovement;
+    public void LeftHandMovement(InputAction.CallbackContext context)
     {
-        
+        handMovement.Move(context.ReadValue<int>());
+        Debug.Log("AAAA");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Satan Claus/Assets/Scripts/Puppets/Movement/HandMovement.cs
+++ Satan Claus/Assets/Scripts/Puppets/Movement/HandMovement.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class HandMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
-    float speed;
+    private Rigidbody2D rb;
+    
+    [SerializeField]
+    private float speed = 10.0f;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
-
     public void Move(int value)
     {
          rb.velocity = Vector2.right * speed * value;
